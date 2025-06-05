49816041f71b1f6510999e46fee559e2315cd23d
Erick Dourado <erickdourado_@hotmail.com>
2025-06-05 12:25:54 -0300

    adding customer example

---
+++ src/main/java/guru/springframework/spring6restmvc/controller/CustomerController.java
@@ -0,0 +1,35 @@
+package guru.springframework.spring6restmvc.controller;
+
+import guru.springframework.spring6restmvc.model.Customer;
+import guru.springframework.spring6restmvc.services.CustomerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/customer")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @GetMapping
+    public List<Customer> listCustomers() {
+        log.debug("List Customers - in controller");
+        return customerService.listCustomers();
+    }
+
+    @GetMapping("/{customerId}")
+    public Customer getCustomerById(@PathVariable("customerId") UUID customerId) {
+        log.debug("Get Customer by Id - in controller");
+        return customerService.getCustomerById(customerId);
+    }
+
+}
